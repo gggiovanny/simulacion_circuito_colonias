f32d320da37a869852e9522c32cc3248b6a1011f
gggiovanny <giovanny.gonzalez.19@gmail.com>
2020-10-26 19:34:36 -0600

    Agregados comentarios faltantes

--- run.py
+++ run.py
@@ -4,22 +4,22 @@ import PetriPy
 
 class Edge:
     def __init__(self, is_traffic_input = False, associated_detector_name = "", num_lanes = 1, aprox_length = 0, aprox_total_width = 0, from_direction = "", to_direction = ""):
-        self.is_traffic_input = is_traffic_input
-        self.associated_detector_name = associated_detector_name
-        self.num_lanes = num_lanes
-        self.aprox_length = aprox_length
-        self.aprox_total_width = aprox_total_width # ancho de la calle completa que incluye a todos los carriles
+        self.is_traffic_input = is_traffic_input # indica si el trafico entra por esta calle
+        self.associated_detector_name = associated_detector_name # nombre del detector de trafico asociado a esta calle
+        self.num_lanes = num_lanes # numero de carriles
+        self.aprox_length = aprox_length # largo aproximado de la calle
+        self.aprox_total_width = aprox_total_width # ancho aproximado de la calle completa que incluye a todos los carriles
         self.aprox_area = aprox_length * aprox_total_width # area calculada
         self.from_direction = from_direction # desde que direccion viene el la calle TODO: implementar enum/dict
         self.to_direction = to_direction # a que direccion va la calle TODO: implementar enum/dict
 
 class Conection:
     def __init__(self, from_edge, to_edge, validate = False):
-        self.from_edge = from_edge
-        self.to_edge = to_edge
-        if validate:
+        self.from_edge = from_edge # desde que calle viene el trafico
+        self.to_edge = to_edge # hacia que calle viene el trafico
+        if validate: # si se deben validar que los parametros recibidos representen una conexión válida. Por defecto es False, para evitar realizar esta operación cuando no sea necesario para ahorrar procesamiento
             self.validate()
-        self.from_edge.is_traffic_input = True
+        self.from_edge.is_traffic_input = True 
         self.to_edge.is_traffic_input = False
     
     def validate(self):
@@ -37,9 +37,9 @@ class Conection:
 
 class Intersection:
     def __init__(self, associated_traffic_light_name, edges_list=[], conections_list=[], ):
-        self.edges_list = edges_list
-        self.conections_list = conections_list
-        self.associated_traffic_light_name = associated_traffic_light_name
+        self.edges_list = edges_list # lista de todas las calles en la intersección. Útil si se quiere recorrer todas una por una
+        self.conections_list = conections_list # lista de todas las conexiones entre calles
+        self.associated_traffic_light_name = associated_traffic_light_name # el nombre del semáforo asociado a la interseccion
 
 # class EventObserver:
 #     def __init__(self):
