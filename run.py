8a6308beb36899b54c62b508b7b0268e6855106c
gggiovanny <giovanny.gonzalez.19@gmail.com>
2020-10-28 19:26:39 -0600

    Creado modelo de la interseccion actual.
    Creadas funciones de impresion de clases __str__.
    
    Correcciones menores.

--- run.py
+++ run.py
@@ -2,8 +2,8 @@ from config import traci
 import config
 import PetriPy
 
-class Edge:
-    def __init__(self, is_traffic_input = False, associated_detector_name = "", num_lanes = 1, aprox_length = 0, aprox_total_width = 0, from_direction = "", to_direction = ""):
+class Edge: # o calle/arista
+    def __init__(self, is_traffic_input = False, associated_detector_name = "", num_lanes = 1, aprox_length = 0, aprox_total_width = 0, name = ""):
         # propiedades
         self.is_traffic_input = is_traffic_input # indica si el trafico entra por esta calle
         self.associated_detector_name = associated_detector_name # nombre del detector de trafico asociado a esta calle
@@ -11,9 +11,11 @@ class Edge:
         self.aprox_length = aprox_length # largo aproximado de la calle
         self.aprox_total_width = aprox_total_width # ancho aproximado de la calle completa que incluye a todos los carriles
         self.aprox_area = aprox_length * aprox_total_width # area calculada
-        self.from_direction = from_direction # desde que direccion viene el la calle TODO: implementar enum/dict
-        self.to_direction = to_direction # a que direccion va la calle TODO: implementar enum/dict
+        self.name = name # nombre o apodo para la calle
         # estado
+    
+    def __str__(self):
+        return self.name
 
 class Conection:
     def __init__(self, from_edge, to_edge, validate = False):
@@ -38,6 +40,9 @@ class Conection:
     
     def typeErrorText(self, kind_of_edge, received_object):
         return 'Error de tipo de datos!', "Se esperaba que {} sea de clase {}, pero se es de tipo {}".format(kind_of_edge, received_object.__class__.__name__, Edge.__name__) 
+    
+    def __str__(self):
+        return "{} -> {}".format(self.from_edge.name, self.to_edge.name)
 
 class Intersection:
     def __init__(self, associated_traffic_light_name, edges_list=[], conections_list=[] ):
@@ -46,12 +51,38 @@ class Intersection:
         self.conections_list = conections_list # lista de todas las conexiones entre calles
         self.associated_traffic_light_name = associated_traffic_light_name # el nombre del semáforo asociado a la interseccion
         # estado
-        
+    
+    def __str__(self):
+        edges_str = ', '.join(map(str, self.edges_list))
+        conections_str = ', '.join(map(str, self.conections_list))
+        return "[{}] controls:\n\tedges: [{}]\n\tconections: [{}]".format(self.associated_traffic_light_name, edges_str, conections_str)
 
 # class EventObserver:
 #     def __init__(self):
 
 #     def observe(self):
+def setup():
+    edges = {
+        "from_north": Edge(num_lanes=2, name='from_north'),
+        "to_south": Edge(num_lanes=2, name='to_south'),
+        "from_east": Edge(num_lanes=2, name='from_east'),
+        "to_west": Edge(num_lanes=2, name='to_west'),
+        "from_west": Edge(num_lanes=2, name='from_west'),
+        "to_east": Edge(num_lanes=2, name='to_east'),
+    }
+    conections = {
+        "north_to_south": Conection(edges["from_north"], edges["to_south"]),
+        "east_to_west": Conection(edges["from_east"], edges["to_west"]),
+        "west_to_east": Conection(edges["from_west"], edges["to_east"]),
+    }
+    intersection = Intersection(
+        "semaforo_circuito_colonias", 
+        edges_list=edges.values(), 
+        conections_list=conections.values()
+    )
+    print(intersection)
+
+
 
 def run():
     t = 0
