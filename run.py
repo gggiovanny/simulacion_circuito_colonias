6fb636f67ae74b5245a87c6b57d03fb11febe4ed
gggiovanny <giovanny.gonzalez.19@gmail.com>
2020-10-28 21:02:33 -0600

    Agregado modelado de la interseccion de cinco colonias a la clase run.

--- run.py
+++ run.py
@@ -1,6 +1,27 @@
 from config import traci
 import config
 import PetriPy
+import models as m
+
+def get_cinco_colonias_intersection():
+    edges = {
+        "from_north": m.Edge(num_lanes=2, name='from_north'),
+        "to_south": m.Edge(num_lanes=2, name='to_south'),
+        "from_east": m.Edge(num_lanes=2, name='from_east'),
+        "to_west": m.Edge(num_lanes=2, name='to_west'),
+        "from_west": m.Edge(num_lanes=2, name='from_west'),
+        "to_east": m.Edge(num_lanes=2, name='to_east'),
+    }
+    conections = {
+        "north_to_south": m.Conection(edges["from_north"], edges["to_south"]),
+        "east_to_west": m.Conection(edges["from_east"], edges["to_west"]),
+        "west_to_east": m.Conection(edges["from_west"], edges["to_east"]),
+    }
+    return m.Intersection(
+        "semaforo_circuito_colonias", 
+        edges_list=edges.values(), 
+        conections_list=conections.values()
+    )
 
 def run():
     t = 0
