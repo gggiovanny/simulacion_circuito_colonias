8d47c4a2bfb6143afabb09941b5c88930136d0d8
gggiovanny <giovanny.gonzalez.19@gmail.com>
2020-11-04 23:01:33 -0600

    Ahora los semaforos se controlan usando redes de petri.

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from config import traci
 import config
-import PetriPy
+import PetriPy as p
 import models as m
 from models import EdgeState
 
@@ -23,11 +23,44 @@ def get_cinco_colonias_intersection():
         edges_list=list(edges.values()), 
         conections_list=list(conections.values())
     )
+    
+def generateTlsPetriNet(tls_name):
+    # generando una lista de objetos Places() (Lugares) en una lista llamada 'places'
+    places = p.generatePlaces(range(4))
+    # repitiendo el mismo proceso para generar los objetos tipo Transition() en la lista 'transition'
+    transition = p.generateTransitions(range(4))
+    # estableciendo las relaciones entre Places y Transitions
+    places[0].addNext(transition[0]) 
+    transition[0].addNext(places[1])
+    places[1].addNext(transition[1])
+    transition[1].addNext(places[2])
+    places[2].addNext(transition[2])
+    transition[2].addNext(places[3])
+    places[3].addNext(transition[3])
+    transition[3].addNext(places[0])
+    # estableciendo tiepos de espera para cada transicion
+    transition[1].wait_time = 30
+    transition[2].wait_time = 6
+    transition[3].wait_time = 30
+    transition[0].wait_time = 6
+    # agregando callbacks en cada transición para que ejecuten un cambio de luces
+    transition[0].action = lambda:traci.trafficlight.setRedYellowGreenState(tls_name, "GGggGGGrrrr")
+    transition[1].action = lambda:traci.trafficlight.setRedYellowGreenState(tls_name, "yyyyyyyrrrr")
+    transition[2].action = lambda:traci.trafficlight.setRedYellowGreenState(tls_name, "rrrrrrrGGGG")
+    transition[3].action = lambda:traci.trafficlight.setRedYellowGreenState(tls_name, "rrrrrrryyyy")
+    # el estado inicial de las marcas
+    initial_state = [1,0,0,0]
+    return p.Network(places, transition, initial_state, 6)
 
 def run():
+    # obteniendo la clase de la interseccion
     intersection = get_cinco_colonias_intersection()
+    # obteniendo la red de petri que controla los semaforos
+    net = generateTlsPetriNet(intersection.associated_traffic_light_name)
+    
     # iniciando la simulacion
     traci.start(['sumo-gui', "-c", config.sumo_data_path+'osm.sumocfg'])
+    
     # obteniendo alguanas propiedades desde la simulacion
     for edge in intersection.edges_list:
         edge.num_lanes = traci.edge.getLaneNumber(edge.name)
@@ -37,6 +70,7 @@ def run():
     # Ejecuta el bucle de control de TraCI
     while traci.simulation.getMinExpectedNumber() > 0:
         traci.simulationStep()
+        net.nextStep()
         # TODO: volver esto un foreach recorriendo todas las calles
         edge_state = m.generateEdgeStateWithTraci(traci, "from_north", t, "normal traffic static rule 25x5")
         
