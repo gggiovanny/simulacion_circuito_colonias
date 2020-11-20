71827dff8bf6dbc2582d5c89d06e5768f51352fb
gggiovanny <giovanny.gonzalez.19@gmail.com>
2020-11-19 21:55:11 -0600

    Agregada nueva red de petri con su respectivo diagrama.

--- demo_fases_dia.py
+++ demo_fases_dia.py
@@ -1,4 +1,43 @@
 from config import traci
 import config
-import PetriPy as p
 import models_db as m
+import petri_nets as nets
+
+def run():
+    # obteniendo los datos de la interseccion del cache 
+    intersection = m.getIntersection("circuito_colonias")
+    # obteniendo las redes de petri que controlan los semaforos
+    net1 = nets.generateDualPetriNet(intersection.associated_traffic_light_name)
+    
+    
+    # iniciando la simulacion
+    traci.start(['sumo-gui', "-c", config.sumo_data_path+'osm.sumocfg'])
+    
+    # # obteniendo alguanas propiedades desde la simulacion
+    m.populateIntersectionUsingTraci(intersection, traci)
+    
+    t = 0
+    wait_between_changes = 0
+    # Ejecuta el bucle de control de TraCI
+    while traci.simulation.getMinExpectedNumber() > 0:
+        estado_anterior = traci.trafficlight.getRedYellowGreenState(intersection.associated_traffic_light_name)
+        # recorriendo todas las calles y generando el estado de cada una
+        m.autoGenerateState(intersection, traci, t, "demo_fases_dia")
+        # avanzando la simulacion
+        traci.simulationStep()
+        # if t < 1200:
+        net1.nextStep(t)
+        # elif t < 2400:
+        # else:
+        t+=1
+        wait_between_changes+=1
+        estado_actual = traci.trafficlight.getRedYellowGreenState(intersection.associated_traffic_light_name)
+        if estado_actual != estado_anterior:
+            print("\t[!]Estado cambiado: {}->{} [t={}, w={}] ({} duró {}s)".format(nets.getStateLabel(state=estado_anterior), nets.getStateLabel(state=estado_actual), t, wait_between_changes, nets.getStateLabel(state=estado_anterior), wait_between_changes))
+            wait_between_changes = 0
+    traci.close()
+
+# este es el punto de entrada al script
+if __name__ == "__main__":
+    # ejecutando la funcion que controla a la simulacion
+    run()
--- petri_nets.py
+++ petri_nets.py
@@ -1,11 +1,11 @@
-import PetriPy as p
+import PetriPy as petri
 from config import traci
 
 def generateDemoTlsPetriNet(tls_name):
     # generando una lista de objetos Places() (Lugares) en una lista llamada 'places'
-    places = p.generatePlaces(range(4))
+    places = petri.generatePlaces(range(4))
     # repitiendo el mismo proceso para generar los objetos tipo Transition() en la lista 'transition'
-    transition = p.generateTransitions(range(4))
+    transition = petri.generateTransitions(range(4))
     # estableciendo las relaciones entre Places y Transitions
     places[0].addNext(transition[0]) 
     transition[0].addNext(places[1])
@@ -27,4 +27,54 @@ def generateDemoTlsPetriNet(tls_name):
     transition[3].action = lambda:traci.trafficlight.setRedYellowGreenState(tls_name, "rrrrrrryyyy")
     # el estado inicial de las marcas
     initial_state = [1,0,0,0]
-    return p.Network(places, transition, initial_state, 6)
+    return petri.Network(places, transition, initial_state, 6)
+
+def generateDualPetriNet(tls_name):
+    NET_LENGTH = 5
+    # generando una lista de objetos Places() (Lugares) en una lista llamada 'places'
+    p = petri.generatePlaces(range(NET_LENGTH))
+    # repitiendo el mismo proceso para generar los objetos tipo Transition() en la lista 'transition'
+    t = petri.generateTransitions(range(NET_LENGTH))
+    # estableciendo las relaciones entre Places y Transitions
+    p[0].addNext(t[0]) 
+    t[0].addNext(p[1])
+    p[1].addNext(t[1])
+    t[1].addNext(p[2])
+    p[2].addNext(t[3])
+    t[3].addNext(p[4])
+    t[1].addNext(p[3])
+    p[3].addNext(t[2])
+    t[2].addNext(p[4])
+    p[4].addNext(t[4])
+    t[4].addNext(p[0])
+    # estableciendo tiepos de espera para cada transicion
+    t[0].wait_time = 35
+    t[1].wait_time = 6
+    t[2].wait_time = 15
+    t[3].wait_time = 25
+    t[4].wait_time = 3
+    # agregando marks extra
+    p[4].required_marks = 2
+    # agregando callbacks en cada transición para que ejecuten un cambio de luces
+    t[0].action = lambda:traci.trafficlight.setRedYellowGreenState(tls_name, getStateLabel(label="yellow")) # yellow
+    t[1].action = lambda:traci.trafficlight.setRedYellowGreenState(tls_name, getStateLabel(label="red")) # red
+    t[4].action = lambda:traci.trafficlight.setRedYellowGreenState(tls_name, getStateLabel(label="green")) # green
+    # el estado inicial de las marcas
+    initial_state = [1,0,0,0,0]
+    return petri.Network(p, t, initial_state, 9)
+
+def getStateLabel(state="", label=""):
+    if state:
+        if state == "yyyyyyyGGGG":
+            return "yellow"
+        elif state == "rrrrrrryyyy":
+            return "red"
+        elif state == "GGggGGGrrrr":
+            return "green"
+    elif label:
+        if label == "yellow":
+            return "yyyyyyyGGGG"
+        elif label == "red":
+            return "rrrrrrryyyy"
+        elif label == "green":
+            return "GGggGGGrrrr"
