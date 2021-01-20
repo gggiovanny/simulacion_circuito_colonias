17d1f9e27d1c18acdd94f0a6de3053bbba810d19
gggiovanny <giovanny.gonzalez.19@gmail.com>
2021-01-20 16:45:18 -0600

    Agregada generación de tráfico de prueba norte a sur.

--- simulacion_dia.py
+++ simulacion_dia.py
@@ -4,19 +4,43 @@ import models_db as m
 import petri_nets as pn
 import traffic_generator as tg
 from scipy.stats import norm
+from scipy.stats import uniform
+import numpy as np
 
 def generateTraffic():
     # time section
-    minute = 20
-    day = minute * 60 * 24
+    minute = 10
+    hour = minute * 60
+    day = hour * 24
     duration = day
-    data_normal = norm.rvs(size=duration,loc=0,scale=0.2)
+    
+    # generando trafico de north to south en las diferentes fases del dia
+    u_loc_medio = 0
+    u_scale_medio = 0.3
+    u_loc_bajo = 0
+    u_scale_bajo = 0.1
+    duracion1 = (int)((hour*5.5)-0)
+    print(duracion1)
+    trafns_0_5 = uniform.rvs(size=duracion1, loc = u_loc_bajo, scale=u_scale_bajo)
+    trafns_5_8 = norm.rvs(size=(int)((hour*8.5)-(hour*5.5)),loc=0,scale=3)
+    trafns_8_12 = uniform.rvs(size=(int)((hour*12.5)-(hour*8.5)), loc = u_loc_bajo, scale=u_loc_medio)
+    trafns_12_15 = norm.rvs(size=(int)((hour*15.5)-(hour*12.5)),loc=0,scale=3)
+    trafns_15_18 = uniform.rvs(size=(int)((hour*18.5)-(hour*15.5)), loc = u_loc_medio, scale=u_scale_medio)
+    trafns_18_21 = norm.rvs(size=(int)((hour*21.5)-(hour*18.5)),loc=0,scale=3)
+    trafns_21_23 = uniform.rvs(size=(int)((hour*23)-(hour*21.5)), loc = u_loc_medio, scale=u_scale_medio)
+    trafns_23_24 = uniform.rvs(size=(int)((hour*24)-(hour*23)), loc = u_loc_bajo, scale=u_scale_bajo)
+    # uniendolo en un solo arreglo
+    trafns_allday = np.concatenate(( trafns_0_5, trafns_5_8, trafns_8_12, trafns_12_15, trafns_15_18, trafns_18_21, trafns_21_23, trafns_23_24 ))
+    # usandolo para generar tráfico
     gen1 = tg.TrafficGenerator("from_north_edge", "to_south_edge", name="test.trafns")
-    gen1.generate(data_normal, vehicle_type="coche")
-    gen2 = tg.TrafficGenerator("from_east_edge", "to_west_edge", name="test.trafew")
-    gen2.generate(data_normal, vehicle_type="coche")
-    gen3 = tg.TrafficGenerator("from_west_edge", "to_east_edge", name="test.trafwe")
-    gen3.generate(data_normal, vehicle_type="coche")
+    gen1.generate(trafns_allday, vehicle_type="coche")
+    
+    
+    
+    # gen2 = tg.TrafficGenerator("from_east_edge", "to_west_edge", name="test.trafew")
+    # gen2.generate(data_normal, vehicle_type="coche")
+    # gen3 = tg.TrafficGenerator("from_west_edge", "to_east_edge", name="test.trafwe")
+    # gen3.generate(data_normal, vehicle_type="coche")
 
 def run():
     # conectando a la base de datos
